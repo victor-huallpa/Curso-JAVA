3fd7afdaa13348d5e00e84235d9adfcaac46329c
victor-huallpa <vech.600@gmail.com>
2026-04-11 15:26:41 -0500

    feat: Validadando edad

---
+++ src/edad.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class edad {
+
+    public static void main(String[] args){
+        String edadS;
+        Integer edad;
+        boolean ver = true;
+
+        Scanner cs = new Scanner(System.in);
+
+        do {
+            System.out.printf("Ingrese su edad:\t");
+            edadS = cs.nextLine();
+
+            if (!edadS.matches("-?\\d+")){
+                System.out.println("Lo siento lo que ingresaste no es un numero valido.\nVuelve a intentarlo.\n");
+                continue;
+            }
+            edad = Integer.parseInt(edadS);
+
+            if (edad.intValue() == 0){
+                System.out.println("Adios!!!\n");
+                ver = false;
+                break;
+            }
+
+            if (edad.intValue() < 18 && edad.intValue() > 0) {
+                System.out.printf("Eres muy wawa regresa al cole!\n");
+            } else if (edad.intValue() >= 18 && edad.intValue() <= 60) {
+                System.out.printf("Eres mayor de edad, me agrada.\n");
+            }else {
+                System.out.printf("Lo siento tu rango de esta fuera de los limites permitidos.\n");
+            }
+        }while (ver);
+
+
+    }
+
+
+
+}
