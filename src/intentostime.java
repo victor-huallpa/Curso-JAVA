05f7207e8f65a58c051aab8b7e79f3b80b142adf
victor-huallpa <vech.600@gmail.com>
2026-04-11 23:15:12 -0500

    refactor: reestructuracion con metodos

--- src/intentostime.java
+++ src/intentostime.java
@@ -3,36 +3,43 @@ import java.util.Scanner;
 public class intentostime {
 
     public static void main(String[] args){
+        boolean validate = true;
+
         int timeP = 3000;
         int timeB = 5000;
-        int pinC = 3456, pinU, contador=0;
+
+        int pinC = 3456, pinU;
         String pin;
+
+        int contador=0;
+        int[] intentos = {5,3,1};
+        int iter = 0;
+
+        String mess1 = "Procesando...";
+
+
         Scanner cs = new Scanner(System.in);
 
 
-        while(contador < 4){
-            if (contador == 3){
-                try{
-                    System.out.println("Fallaste 3 veces vuelve a intentarlo dentro de 30 segundo.");
-                    contador = 0;
-                    Thread.sleep(timeB);
-                }catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-            }
-            System.out.println("-----------------");
-            System.out.printf("Intentos: %d\n",3-contador);
-            System.out.println("-----------------");
-            System.out.println("Ingrese su PIN: ");
-            pin = cs.nextLine();
+        while(validate){
+            String mess2 = "Fallaste " +  intentos[iter] + " veces vuelve a intentarlo dentro de 30 segundo.\n";
 
-            //teimpo de procesado..
-            try {
-                System.out.println("Procesando...");
-                Thread.sleep(timeP);
-            }catch (InterruptedException e){
-                e.printStackTrace();
+            if (contador >= intentos[iter]){
+                //validando iteraciones
+                if (iter == 2){
+                    System.out.println("los siento alcanasaste el limite de intentos\nContactece con el area encargada.");
+                    validate = false;
+                    break;
+                }
+                //teimpo de blokeo..
+                proccesTime(timeB, mess2);
+                iter++;
+                contador = 0;
+
             }
+
+            pin = datos(cs, contador, intentos[iter]);
+
 
             if (!pin.matches(" ?\\d+")){
                 System.out.println("Lo siento no puedes ingresar caracteres ni texto, solo numeros!\nVuelva a intentarlo.\n");
@@ -40,17 +47,52 @@ public class intentostime {
                 continue;
             }
 
+            //teimpo de procesado..
+            proccesTime(timeP, mess1);
+
             //parseando el string a entero
             pinU = Integer.parseInt(pin.trim());
 
-            if (pinU == pinC){
-                System.out.println("Desbloqueado!...");
+            //validar desblokeo
+            if (validateUser(pinC, pinU)) {
+                contador++;
+            } else {
                 break;
-            }else {
-                System.out.println("PIN incorrecto\nVuelva a intentarlo.\n");
             }
-            contador++;
 
         }
     }
+
+    //pedir datos y msotar intentos
+    private static String datos(Scanner cs, int contador, int intentos){
+        System.out.println("-----------------");
+        System.out.printf("Intentos: %d\n",intentos-contador);
+        System.out.println("-----------------");
+        System.out.println("Ingrese su PIN: ");
+
+        return cs.nextLine();
+    }
+
+    //Tiempo para procesar
+    private static void proccesTime(int tiempo, String messages){
+        try {
+            System.out.println(messages);
+            Thread.sleep(tiempo);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    //validando Ingreso de usuario
+    private static boolean validateUser(int pin, int pinU){
+        if (pinU == pin){
+            System.out.println("Desbloqueado!...");
+            return false;
+        }else {
+            System.out.println("PIN incorrecto\nVuelva a intentarlo.\n");
+            return true;
+        }
+    }
+
+
 }
