d6691ccfc7a7405be0737c2048b8685daf5cd7bb
victor-huallpa <vech.600@gmail.com>
2026-04-13 13:15:50 -0500

    feat: Invertir arreglo

---
+++ src/ejercicios/InvertirArr.java
@@ -0,0 +1,22 @@
+package ejercicios;
+
+public class InvertirArr {
+
+    public void inver(int[] arr){
+        int temp ;
+        int numIter = (arr.length/2);
+        for (int i = 0; i < numIter; i++) {
+
+                temp = arr[i];
+                arr[i] = arr[arr.length-1-i];
+                arr[arr.length-1-i] = temp;
+        }
+
+    }
+
+    public static void iter(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d, ",arr[i]);
+        }
+    }
+}
