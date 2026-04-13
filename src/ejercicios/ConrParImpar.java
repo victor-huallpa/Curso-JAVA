908ddf88ae436d570e98fe8cb38fcc1486beaa36
victor-huallpa <vech.600@gmail.com>
2026-04-13 13:16:27 -0500

    feat: Contara parers eh impares

--- src/ejercicios/ConrParImpar.java
+++ src/ejercicios/ConrParImpar.java
@@ -1,4 +1,17 @@
 package ejercicios;
 
 public class ConrParImpar {
+
+    public static int[] contParImpar(int[] arr){
+        int[] parImp = {0,0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                parImp[0] ++;
+                continue;
+            }
+            parImp[1]++;
+        }
+
+        return parImp;
+    }
 }
