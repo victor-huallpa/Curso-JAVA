4a46959d19a502d7525efa42ad778026f3236ccc
victor-huallpa <vech.600@gmail.com>
2026-04-12 23:03:00 -0500

    fead: nuevo main

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+import learning.Arreglos;
+public class main {
+    public static void main(String[] args){
+
+        Arreglos arr = new Arreglos();
+
+        arr.arrBi();
+
+        System.out.println("Hello!!");
+    }
+}
