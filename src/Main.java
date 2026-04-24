9aa04d92248b7ab6b615bc7835aca60ff0d7b748
victor-huallpa <vech.600@gmail.com>
2026-04-24 17:23:26 -0500

    reproduciend oel ble de la iteracion

--- src/Main.java
+++ src/Main.java
@@ -67,6 +67,8 @@ public class Main {
 
         for (int i = 0; i < 6; i++) {
             System.out.println(i);
+            System.out.println(i);
+            System.out.println(i);
         }
         
     }
