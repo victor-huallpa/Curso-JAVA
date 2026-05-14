e961c34a9e8063c452e7d9e38523e6f2804db20e
victor-huallpa <vech.600@gmail.com>
2026-05-14 07:50:43 -0500

    feat: Mi primer formulario

---
+++ pryMiFrmPrin/src/main/java/jpForm.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class jpForm {
+    public JPanel jpPrinci;
+    private JTextField jpText;
+    private JButton btnMostrar;
+    private JLabel jpNom;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
